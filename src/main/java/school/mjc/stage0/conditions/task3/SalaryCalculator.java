a270289c7317cd1db9a0ab8623417e25ce556d62
atainaive <atai.omorov@stud.vgtu.lt>
2022-06-28 11:08:59 +0300

    commit

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,19 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        double afterTaxes = 0;
+
+        if (salary <= 0) {
+            System.out.println("wrong input!");
+        } else if (salary <= 10000) {
+            afterTaxes = salary - (0.15 * salary);
+            System.out.println(afterTaxes);
+        } else if (salary > 10000 && salary <= 20000) {
+            afterTaxes = salary - (0.18 * salary);
+            System.out.println(afterTaxes);
+        } else if (salary > 20000) {
+            afterTaxes = salary - (0.2 * salary);
+            System.out.println(afterTaxes);
+        }
     }
 }
